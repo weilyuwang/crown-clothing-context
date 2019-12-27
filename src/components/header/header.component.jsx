65485937a98c0c40c164b044fe971fc6c76e4d6b
weilyuwang <weilyuwang@gmail.com>
2019-12-26 20:39:36 -0500

    refactor the rest pages&components to use context api

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,12 +8,15 @@ import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import { selectCartHidden } from "../../redux/cart/cart.selectors";
 import CurrentUserContext from "../../contexts/current-user/current-user.context";
+import { CartContext } from "../../providers/cart/cart.provider";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 import "./header.styles.scss";
 
-const Header = ({ hidden }) => {
+const Header = () => {
   const currentUser = useContext(CurrentUserContext);
+  const { hidden } = useContext(CartContext);
+
   return (
     <div className="header">
       <Link className="logo-container" to="/">
